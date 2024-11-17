d839399aa89e1f8d9fd6be479ff6547059d2a711
Rokibul Alom <itismehey975@gmail.com>
2024-11-18 00:39:17 +0600

    navbar and footer added successfully

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -3,7 +3,33 @@
 const Footer = () => {
     return (
         <div>
-            this is footer
+            <footer className=" bg-neutral text-neutral-content p-10">
+                <div className="my-4">
+                    <h1 className="font-bold text-2xl text-center text-white">NextStep Navigator </h1>
+                </div>
+                <div className="footer">
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                </div>
+            </footer>
         </div>
     );
 };
